e89436891ac8a492595fde1128cf10d5a1ceb30b
FrederickDrascic <frederick.j.drascic@odfw.oregon.gov>
2025-07-09 13:37:41 -0700

    Updated saveRDS to save to data folder instead of main folder.

--- pit_antenna_dashboard_data.R
+++ pit_antenna_dashboard_data.R
@@ -173,6 +173,6 @@ uptime <- uptime |>
 Timer_tags <- reactable(uptime, defaultPageSize = 25)
 Timer_tags
 
-saveRDS(Timer_tags, "C:/Users/glassefr/OneDrive - Oregon/PIT Antenna Project/Shiny Apps/Fish_Dashboard/uptime_data.rds")
-saveRDS(antenna_plot, "C:/Users/glassefr/OneDrive - Oregon/PIT Antenna Project/Shiny Apps/Fish_Dashboard/antenna_plot.rds")
+saveRDS(Timer_tags, "C:/Users/glassefr/OneDrive - Oregon/PIT Antenna Project/Shiny Apps/Fish_Dashboard/data/uptime_data.rds")
+saveRDS(antenna_plot, "C:/Users/glassefr/OneDrive - Oregon/PIT Antenna Project/Shiny Apps/Fish_Dashboard/data/antenna_plot.rds")
 
